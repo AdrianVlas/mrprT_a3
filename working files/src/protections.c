7abb5c2aaf7c9acc0d64ffdde65acf9c244d99c7
Taras Malynovsky <malynovsky@ukr.net>
2018-11-22 15:55:17 +0200

    22.11.18: Вніс корекції після об'єднання

--- working files/src/protections.c
+++ working files/src/protections.c
@@ -6542,7 +6542,6 @@ inline void analog_registrator(unsigned int* carrent_active_functions)
 }
 /*****************************************************/
 
-char ch_type_voltage = 0;
 /*****************************************************/
 //Функція захистів з якої здійснюються всі інші операції
 /*****************************************************/
@@ -6582,7 +6581,6 @@ inline void main_protection(void)
       
       information_about_restart_counter  &= (unsigned int)(~(1 << RS485_RECUEST));
     }
-    ch_type_voltage = (current_settings.control_transformator >> INDEX_ML_CTR_TRANSFORMATOR_VH_VL) & 0x1;
     //Помічаємо що ми виконали очистку по ВСІХ інтерфейсах
     reset_trigger_function_from_interface = 0;
   }
@@ -7063,6 +7061,12 @@ inline void main_protection(void)
   }
   /**************************/
 
+  /**************************/
+  //Вибір типу напруги підведеної до приладу
+  /**************************/
+  ch_type_voltage = (current_settings.control_transformator >> INDEX_ML_CTR_TRANSFORMATOR_VH_VL) & 0x1;
+  /**************************/
+  
   /**************************/
   //Вибір групи уставок
   /**************************/
