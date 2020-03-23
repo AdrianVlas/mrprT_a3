01bbe66be9d82bcf970fd0dcf8e8720bb6750bec
Taras Malynovsky <malynovsky@ukr.net>
2020-03-23 15:26:21 +0200

    23.03.20: Збільшив коефіцієнт повернення для напрасленості МЗС по струму до 1,3 відсотка

--- working files/inc/const_koef_povernennja.h
+++ working files/inc/const_koef_povernennja.h
@@ -6,7 +6,7 @@
 #define KOEF_POVERNENNJA_3I0_OTHER      85
 #define KOEF_POVERNENNJA_ZOP_BLK        120
 
-#define KOEF_POVERNENNJA_I_SECTOR_BLK     120
+#define KOEF_POVERNENNJA_I_SECTOR_BLK     130
 #define KOEF_POVERNENNJA_U_SECTOR_BLK     110
 
 #define U_UP                            90
